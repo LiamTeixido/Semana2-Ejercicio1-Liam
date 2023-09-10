c18c3ae475316e9be1e98acc94d788a8e5f380fd
LiamTeixido <114273617+LiamTeixido@users.noreply.github.com>
2023-09-10 15:28:31 -0500

    Creación del Proyecto - Liam Teixido

---
+++ Semana2_Ejercicio1_LiamTeixido/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana2_Ejercicio1_LiamTeixido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
